92ecd91aecd475e957021596b4f770e712277be2
Adrian Olszowski <olszowski@europa-tu.local>
2019-04-26 15:10:19 +0200

    Initialations server conf

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 require(__dirname + '/Resources/config.js');
 
-console.log(config.database);
+const fs = require('fs');
+const net = require('net');
+
+const initFiles = fs.readdirSync(__dirname + "/Initializers");
+initFiles.forEach((initFiles)=>{
+    console.log("Load init" + initFiles);
+    require(__dirname + "/Initializers/" + initFiles);
+})
+
+const modelFiles = fs.readdirSync(__dirname + "/Models");
+modelFiles.forEach((modelFile)=>{
+    console.log("Load Model" + modelFile);
+    require(__dirname + "/Models/" + modelFile);
+})
+
+let maps = {}
+const mapsFiles = fs.readdirSync(config.data_paths.maps);
+mapsFiles.forEach((mapsFile)=>{
+    console.log("Load Maps" + mapsFile);
+    const map = require(config.data_paths.maps + mapsFile);
+    maps[map.room] = map
+})
+
+
+console.log(maps);
+// maps.rm_start_town.

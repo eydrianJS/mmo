5e13383fdb7119879d7fc87e1bcd5e3b907066db
Adrian Olszowski <olszowski@europa-tu.local>
2019-04-26 14:53:07 +0200

    configuration system

---
+++ Resources/config.js
@@ -0,0 +1,36 @@
+const args = require('minimist')(process.argv.slice(2));
+const extend = require('extend');
+
+const environment = args.env || "test";
+
+const common_conf = {
+    name: "game",
+    version: "0.0.1",
+    environment: environment,
+    max_player: 100,
+    data_paths: {
+        items: __dirname + "\\GameData\\" + "\\Items\\",
+        maps: __dirname + "\\GameData\\" + "\\Maps\\",
+    },
+    start_zone: "rm_map_home"
+}
+
+const conf = {
+    
+    production: {
+        ip: args.ip || "0.0.0.0",
+        port: args.port || 8081,
+        database: "mongodb://127.0.0.1/game_prod"
+    },
+    
+    test: {
+        ip: args.ip || "0.0.0.0",
+        port: args.port || 8082,
+        database: "mongodb://127.0.0.1/game_test"
+    }
+};
+
+extend(false, conf.production, common_conf);
+extend(false, conf.test, common_conf);
+
+module.exports = config = conf[environment];
---
+++ server.js
@@ -0,0 +1,3 @@
+require(__dirname + '/Resources/config.js');
+
+console.log(config.database);
